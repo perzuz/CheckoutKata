f1510c83cbe1bf64affacde0711802541cff5c39
Harris Scott <harrisscott@outlook.com>
2022-07-21 00:14:39 +0100

    Remove: unused IPromotion interface
    Add: a simple user input mode to program.cs allowing users to add items and checkout through console

--- CheckoutKata/Program.cs
+++ CheckoutKata/Program.cs
@@ -2,6 +2,7 @@
 using CheckoutKata.DataRepositories;
 using CheckoutKata.Interfaces;
 using CheckoutKata.Promotions;
+using CheckOutKata.Models;
 
 var promotions = new List<Promotion>()
 {
@@ -10,13 +11,11 @@ var promotions = new List<Promotion>()
 };
 
 var promotionManager = new PromotionManager(promotions);
-
 IItemStore itemStore = new SampleItemStore();
 var basket = new Basket();
 
-basket.AddItem(itemStore.GetItem("A"), 1);
-basket.AddItem(itemStore.GetItem("B"), 5);
-basket.AddItem(itemStore.GetItem("Z"), 3);
+//RunSampleMode(basket);
+RunUserInputMode(basket);
 
 var checkout = new Checkout(basket, promotionManager);
 
@@ -24,3 +23,59 @@ var total = checkout.TotalCost();
 
 Console.WriteLine($"Total of basket is £{total}");
 Console.Read();
+
+void RunSampleMode(Basket basket)
+{
+    basket.AddItem(itemStore.GetItem("A"), 1);
+    basket.AddItem(itemStore.GetItem("B"), 5);
+    basket.AddItem(itemStore.GetItem("D"), 3);
+}
+
+void RunUserInputMode(Basket basket)
+{
+    Console.WriteLine("Welcome to the Checkout!" + Environment.NewLine);
+    Console.WriteLine("Add your desired items to the basket below and type \"checkout\" when you are finished" + Environment.NewLine);
+
+    while (Console.ReadLine() != "checkout")
+    {
+        Item itemToAdd = null;
+        var quantity = 0;
+
+        var itemFound = false;
+        while (!itemFound)
+        {
+            Console.WriteLine("Enter an item SKU: ");
+            string skuInput = Console.ReadLine();
+
+            try
+            {
+                itemToAdd = itemStore.GetItem(skuInput);
+
+                itemFound = true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        var quantityValid = false;
+        while (!quantityValid)
+        {
+            Console.WriteLine("Enter desired quantity: ");
+            string quantityInput = Console.ReadLine();
+
+            try
+            {
+                quantity = Convert.ToInt32(quantityInput);
+                quantityValid = true;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("please enter a valid quantity");
+            }
+        }
+
+        basket.AddItem(itemToAdd, quantity);
+    }
+}
